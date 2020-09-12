03da51a976c0bcbb61d49c62a8892cdbc4332cc5
Kaushal Rohit <rohit.kg98@gmail.com>
2020-09-12 17:36:51 +0530

    Updates

---
+++ precache-manifest.f4ce49f8f518cecb714d8b755d582447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac7850918c0f27fe126397aea5bc9ce",
+    "url": "/Abhiyanta-Community.github.io/index.html"
+  },
+  {
+    "revision": "baa7150280a5d2e95923",
+    "url": "/Abhiyanta-Community.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1abf78ca989a60efab7",
+    "url": "/Abhiyanta-Community.github.io/static/js/2.5c7c9de4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Abhiyanta-Community.github.io/static/js/2.5c7c9de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa7150280a5d2e95923",
+    "url": "/Abhiyanta-Community.github.io/static/js/main.41a13239.chunk.js"
+  },
+  {
+    "revision": "d2aca621f7a69e5a5612",
+    "url": "/Abhiyanta-Community.github.io/static/js/runtime-main.b4c6bfba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Abhiyanta-Community.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
