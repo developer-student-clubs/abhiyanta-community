adbd4a5855d25e69a79ed9d3fc4d477be42f9a0d
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2020-10-14 15:26:16 +0530

    changed project page format and aligned if properly

--- src/Components/ProjectMemberCard.js
+++ src/Components/ProjectMemberCard.js
@@ -1,56 +1,57 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 
-import Rating from "@material-ui/lab/Rating";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
+  align: {
+    position: "relative",
+    top: "50px",
+    left:"100px"
+  }
 });
 
 function ImgMediaCard(props) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.align}>
       <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt={props.member.name}
-            height="140"
-            image={props.member.image}
-            title={props.member.name}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {props.member.name}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button href="#" size="small" color="primary">
-            More Details
-          </Button>
-          <Rating
-            value={props.member.rating}
-            size={"small"}
-            precision={0.5}
-            readOnly
-          />
-        </CardActions>
-      </Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Project Image"
+          height="140"
+          image={props.member.image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            <b>{props.member.title}</b>
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.member.content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
     </div>
   );
 }
--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -3,52 +3,58 @@ import React from "react";
 import ProjectMemberCard from "../Components/ProjectMemberCard";
 import { Grid, Container } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
-const Items = [
+
+const project = [
   {
-    rank: 0,
-    rating: 2.5,
-    name: "project1",
+    title: "Diode",
     image:
-      "https://ssl.gstatic.com/analytics/20200901-00/app/static/mobile/ZeroStateIcons/zero-state-users.png",
+      "https://image.freepik.com/free-vector/light-blue-project-management-concept_23-2147782704.jpg",
+    content: "There are many types of semiconductor diodes namely Selenium, Germanium and Silicon types. Selenium type is commonly used in the early days in ac power suppliers but in recent years... "
   },
   {
-    rank: 1,
-    rating: 4,
-    name: "Project2",
+    title: "Diode",
     image:
-      "https://ssl.gstatic.com/analytics/20200901-00/app/static/mobile/ZeroStateIcons/zero-state-users.png",
+      "https://image.freepik.com/free-vector/light-blue-project-management-concept_23-2147782704.jpg",
+    content: "There are many types of semiconductor diodes namely Selenium, Germanium and Silicon types. Selenium type is commonly used in the early days in ac power suppliers but in recent years... "
   },
   {
-    rank: 0,
-    rating: 2.5,
-    name: "project3",
+    title: "Diode",
     image:
-      "https://ssl.gstatic.com/analytics/20200901-00/app/static/mobile/ZeroStateIcons/zero-state-users.png",
+      "https://image.freepik.com/free-vector/light-blue-project-management-concept_23-2147782704.jpg",
+    content: "There are many types of semiconductor diodes namely Selenium, Germanium and Silicon types. Selenium type is commonly used in the early days in ac power suppliers but in recent years... "
   },
   {
-    rank: 1,
-    rating: 4,
-    name: "Project4",
+    title: "Diode",
     image:
-      "https://ssl.gstatic.com/analytics/20200901-00/app/static/mobile/ZeroStateIcons/zero-state-users.png",
+      "https://image.freepik.com/free-vector/light-blue-project-management-concept_23-2147782704.jpg",
+    content: "There are many types of semiconductor diodes namely Selenium, Germanium and Silicon types. Selenium type is commonly used in the early days in ac power suppliers but in recent years... "
   },
+  {
+    title: "Diode",
+    image:
+      "https://image.freepik.com/free-vector/light-blue-project-management-concept_23-2147782704.jpg",
+    content: "There are many types of semiconductor diodes namely Selenium, Germanium and Silicon types. Selenium type is commonly used in the early days in ac power suppliers but in recent years... "
+  },
+
 ];
 
+
 const useStyles = makeStyles((theme) => ({
-  align: {
-    position: "relative",
-    top: "140px"
+  pad: {
+    marginBottom:"10%",
+    marginTop:"5%",
+
   }
 }));
 
 
-function Project() {
+function Team() {
   const classes = useStyles();
   return (
-    <div className={classes.align}>
+    <div className={classes.pad}>
       <Container>
         <Grid container spacing={3}>
-          {Items.map((member) => (
+          {project.map((member) => (
             <Grid item xs={12} sm={6} md={4}>
               <ProjectMemberCard member={member} />
             </Grid>
@@ -60,4 +66,4 @@ function Project() {
   );
 }
 
-export default Project;
+export default Team;
