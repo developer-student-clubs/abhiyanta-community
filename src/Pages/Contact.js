4daa217bec2ae367896c0e3ecc085118653ac465
ksidhpuri <ksidhpuri@gmail.com>
2020-10-19 18:32:59 +0530

    modify: contact form url

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -6,10 +6,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     form: {
-        height: "1100px",
         position: "relative",
-        left: "590px",
-        top: "140px"
+        top: 100,
+        textAlign: "center"
     }
 }));
 
@@ -18,11 +17,11 @@ function Contact() {
     const classes = useStyles();
 
     return (
-
-        <iframe title="contactPage" className={classes.form} src="https://docs.google.com/forms/d/e/1FAIpQLSeunMIVOtBwmVF-IFLI924A6XCPPg2R52fwK5iWC1UX_K_cNg/viewform?embedded=true" width="640" height="673" frameborder="0" marginheight="0" marginwidth="0">
+        <div className={classes.form}>
+        <iframe title="contactPage"  src="https://docs.google.com/forms/d/e/1FAIpQLSfRYb0yOlHnSjKP0Ogck2uiJitoieJrUv62ZOOXQFJzpxSIuA/viewform?embedded=true" width="640" height="950" frameborder="0" marginheight="0" marginwidth="0">
 
         </iframe>
-
+        </div>
     );
 }
 
