29ef2dbd820fbf3a812fe0cffc81f69c4cd2bf51
squarebat <ksidhpuri@gmail.com>
2020-12-11 15:21:06 +0530

    feat: add careers page

---
+++ src/Components/Position.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Grid, Box, IconButton } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+let fonttheme = createMuiTheme();
+fonttheme.typography.body1 = {
+    fontSize: '0.85rem',
+}
+const useStyles = makeStyles((theme) => ({
+    divBox: {
+        backgroundColor: '#252729',
+        borderRadius: '10px',
+        padding: '20px',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    icon: {
+        color: "white",
+    },
+}));
+
+function PositionDiv(props) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <div>
+            <div>
+                <h4>{props.position.name}
+                <IconButton
+                    className={clsx(classes.expand, classes.icon, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+                </h4>
+                <hr/>
+            </div>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <div className={classes.divBox}>
+                <p>
+                    {props.position.description}
+                </p>
+                <div>
+                    <h6>Key Responsibilities</h6>
+                    <ul>
+                        {props.position.responsibility.map((text) =>
+                            <li>{text}</li>
+                        )}
+                    </ul>
+                </div>
+                <div>
+                    <h6>Basic Qualifications</h6>
+                    <ul>
+                        {props.position.basic_qualification.map((text) =>
+                            <li>{text}</li>
+                        )}
+                    </ul>
+                </div>
+                <div>
+                    <h6>Preferred Qualifications</h6>
+                    <ul>
+                        {props.position.pref_qualification.map((text) =>
+                            <li>{text}</li>
+                        )}
+                    </ul>
+                </div>
+                { props.position.advantages.length>0 ?                     
+                    <div>
+                        <h6>What will you get?</h6>
+                        <ul>
+                            {props.position.advantages.map((text) =>
+                                <li>{text}</li>
+                            )}
+                        </ul>
+                    </div>
+                  : null  
+                }
+                </div>
+            </Collapse>
+        </div>
+    );
+}
+
+export default PositionDiv;
---
+++ src/Pages/Career.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Position from "../Components/Position";
+import { Container, Box, Hidden } from "@material-ui/core";
+import { Careers } from "../Content/CareersContent";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  align: {
+    position: "relative",
+    top: '30px',
+  },
+  tagline: {
+      fontSize: '20px',
+      fontStyle: 'italic'
+  },
+  box: {
+    borderRadius: '10px',
+    height: '500px',
+    width: '100%',
+    overflow: 'hidden'
+  }
+}));
+function DisplayCareers() {
+  const classes = useStyles();
+  return (
+    <div className={classes.align}>
+      <Container maxWidth="md">        
+        <h3>{Careers[0].title}</h3>        
+        <p className={classes.tagline}>
+          {Careers[0].tagline}
+        </p>
+        {Careers[0].positions.map((position, index) => (
+          <Position position = {position} index = {index}/>      
+        ))}
+        <br/>
+        <center>
+        <iframe title="ApplyPage" className={classes.box} src="https://abhiyantaind.typeform.com/to/CX02FeRM" frameborder="0" marginheight="0" marginwidth="0">
+
+        </iframe>
+        </center>        
+      </Container>
+    </div>
+  );
+}
+export default DisplayCareers;
