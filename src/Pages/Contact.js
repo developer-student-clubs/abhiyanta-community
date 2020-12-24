3fbf6141a58f934642fb07c6a501ea334b0ff845
Utsav <39943143+utsav00@users.noreply.github.com>
2020-12-24 13:36:08 +0530

    Solved: Misalignment and open endpoint issue; Updated: Career page details(#47)
    * Make Responsive Contact Us page
    
    * Commit
    
    * Update Faq.css
    
    Make padding as per Hosted version
    
    * Revert last three commits
    
    * Careers Page Added (#42)
    
    * fix: dir structure
    
    * changes in debug.log
    
    * pull from develop
    
    * feat: add careers page
    
    * minor fixes
    
    * modify: colors
    
    * modify: color tweaks
    
    * fix: build error
    
    Co-authored-by: Khadija Sidhpuri <squarebat@pop-os.localdomain>
    
    * Updated Contact Us Page (#44)
    
    * clone latest develop branch and apply faq.css and contact.css to make responsive
    
    * remove (master slave) from content in faq
    
    * Issue solved -Faq link
    
    * remove final contact us form align bug
    
    * Update Contact.css
    
    * Updated: Career page details
    
    Co-authored-by: Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
    Co-authored-by: Arvik <58507059+Bhavik-Ardeshna@users.noreply.github.com>
    Co-authored-by: Utsav <utsav@pop-os.localdomain>
    Co-authored-by: Khadija Sidhpuri <59063821+squarebat@users.noreply.github.com>
    Co-authored-by: Khadija Sidhpuri <squarebat@pop-os.localdomain>
    Co-authored-by: ishita0202 <ishitaachauhan11@gmail.com>

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import CustomizedAccordions from './Faq';
 import "./Contact.css";
+import { Container } from '@material-ui/core';
 
 
 function Contact() {
     return (
         <div>
-
             < CustomizedAccordions />
+            <Container>
             <iframe title="contactPage" className="form" src="https://form.typeform.com/to/NmQXsN32" frameborder="0" marginheight="0" marginwidth="0">
-
             </iframe>
+            </Container>
         </div>
     );
 }
-
-
-
 
 export default Contact;
