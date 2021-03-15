9722b00ffcb70b1ca779e483121d03877b9178a5
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-15 18:56:41 +0530

    change form width to 100%

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -5,6 +5,10 @@ import { Container, Paper, Button } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -16,8 +20,8 @@ const useStyles = makeStyles((theme) => ({
     },
     formControl: {
         margin: theme.spacing(1),
-        minWidth: 300,
-        maxWidth: 300,
+        minWidth: " 100%",
+
     },
     chips: {
         display: 'flex',
@@ -30,12 +34,12 @@ const useStyles = makeStyles((theme) => ({
         marginTop: theme.spacing(3),
     },
     pad: {
-
+        paddingRight: 10,
         paddingBottom: 40
     },
     paper: {
-        minWidth: 400,
-        maxWidth: 300,
+        minWidth: "100%",
+        maxWidth: "100%",
         padding: 20,
         marginLeft: 'auto',
         marginRight: 'auto',
@@ -43,6 +47,9 @@ const useStyles = makeStyles((theme) => ({
     },
     formpad: {
         paddingTop: 20
+    },
+    inputwidth: {
+        width: "100%",
     }
 }));
 
@@ -56,38 +63,39 @@ function Contact() {
         query: ""
     });
 
-    const {name,phno,email,query} = data;
+    const { name, phno, email, query } = data;
 
     const handleChange = (event) => {
-    SetData({ ...data, [event.target.name]: event.target.value});
+        SetData({ ...data, [event.target.name]: event.target.value });
     };
 
     const handleSubmit = async (event) => {
-    event.preventDefault();
+        event.preventDefault();
         try {
             const response = await fetch(
-            "https://v1.nocodeapi.com/squarebat/google_sheets/MMUvHVCDOmWjmFcg?tabId=Sheet2", {
+                "https://v1.nocodeapi.com/squarebat/google_sheets/MMUvHVCDOmWjmFcg?tabId=Sheet2", {
                 method: 'POST',
                 headers: {
-                'Content-Type': 'application/json'
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify([
-                    [new Date().toDateString(),name,phno,email,query]
+                    [new Date().toDateString(), name, phno, email, query]
                 ])
             }
             );
             await response.json();
-            SetData({ ...data, 
-            name: "",
-            phno: "",
-            email: "",
-            query: ""
+            SetData({
+                ...data,
+                name: "",
+                phno: "",
+                email: "",
+                query: ""
             });
         } catch (err) {
             console.log(err);
         }
     };
-       return (
+    return (
         <div>
 
             < CustomizedAccordions />
@@ -96,21 +104,56 @@ function Contact() {
                 {/* <iframe title="contactPage" className="form" src="https://form.typeform.com/to/NmQXsN32" frameborder="0" marginheight="0" marginwidth="0">
                 </iframe> */}
                 <Paper className={classes.paper} elevation={3}>
-                    <FormControl className={classes.formControl}>
+                    <FormControl variant="outlined" className={classes.formControl} >
                         <div className={classes.formpad}>
-
+                            <div className={classes.pad}>
+                                <InputLabel className={classes.formpad} htmlFor="outlined-age-native-simple">Position</InputLabel>
+                                <Select
+                                    native
+                                    // value={state.age}
+                                    // onChange={handleChange}
+                                    className={classes.inputwidth}
+                                    label="Position"
+                                    inputProps={{
+                                        name: "position",
+                                        id: "outlined-age-native-simple"
+                                    }}
+                                >
+                                    <option aria-label="None" value="" />
+                                    <option value={10}>Technical Lead</option>
+                                    <option value={20}>Non-Technical Lead</option>
+                                    <option value={30}>Innovators</option>
+                                </Select>
+                                {/* <InputLabel htmlFor="outlined-age-native-simple">Position</InputLabel>
+                                <Select
+                                    native
+                                    // value={state.age}
+                                    // onChange={handleChange}
+                                    className={classes.inputwidth}
+                                    label="position"
+                                    inputProps={{
+                                        name: 'position',
+                                        id: 'outlined-age-native-simple',
+                                    }}
+                                >
+                                    <option aria-label="None" value="" />
+                                    <option value={10}>Technical Lead</option>
+                                    <option value={20}>Non-Technical Lead</option>
+                                    <option value={30}>Innovators</option>
+                                </Select> */}
+                            </div>
                             <div className={classes.pad}>
 
-                                <TextField iid="outlined-multiline-static"
+                                <TextField iid="outlined-multiline-static" className={classes.inputwidth}
                                     onChange={handleChange} variant="outlined" required id="name" name="name" value={name} label="Name" />
                             </div>
                             <div className={classes.pad}>
 
-                                <TextField iid="outlined-multiline-static"
+                                <TextField iid="outlined-multiline-static" className={classes.inputwidth}
                                     onChange={handleChange} variant="outlined" required id="phno" name="phno" value={phno} label="Contact No" /> </div>
                             <div className={classes.pad}>
 
-                                <TextField iid="outlined-multiline-static"
+                                <TextField iid="outlined-multiline-static" className={classes.inputwidth}
                                     onChange={handleChange} variant="outlined" required id="email" name="email" value={email} label="Email Id" />  </div>
                             <div className={classes.pad}>
 
@@ -120,15 +163,16 @@ function Contact() {
                                     label="Type your query here..."
                                     multiline
                                     rows={4}
-                                    name ="query"
+                                    name="query"
                                     value={query}
                                     onChange={handleChange}
+                                    className={classes.inputwidth}
                                 />
                             </div>
                         </div>
                         <div className={classes.pad}>
-                          <Button type="button" variant="contained" onClick={handleSubmit} color="primary">
-                            Submit
+                            <Button type="button" variant="contained" onClick={handleSubmit} color="primary">
+                                Submit
                           </Button>
                         </div>
                     </FormControl>
@@ -136,7 +180,7 @@ function Contact() {
             </Container>
 
 
-        </div>
+        </div >
     );
 }
 
