c48473ed2ec5d6009b78d513fd42f47ade13a8f1
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-02-25 15:02:52 +0530

    fixed multiline warning

--- src/Content/EventContent.js
+++ src/Content/EventContent.js
@@ -23,10 +23,7 @@ export const event = [
         title: "Introduction to Abhiyanta ",
         title2:"(30th Sept 2021)",
         image: process.env.PUBLIC_URL + "/Assets/Event/current/7.jpg",
-        content: "Hello innovators !! \nAbhiyanta Community strives to create an environment for innovators and enthusiasts, where they learn, innovate and implement their ideas together. We intend to facilitate overall growth of individuals by collaborating with other platforms. We also maintain a creative work environment, which respects diversity, new ideas and hard work. We are pleased to introduce our community and share our experience.\
-        \nDate: 30th Sept 2021 (8:45 P.M. Onwards) \
-        \nLast Date of Registration: 29th Sept 2021 \
-        \nPlatform: Google Meet",
+        content: "Hello innovators !! \nAbhiyanta Community strives to create an environment for innovators and enthusiasts, where they learn, innovate and implement their ideas together. We intend to facilitate overall growth of individuals by collaborating with other platforms. We also maintain a creative work environment, which respects diversity, new ideas and hard work. We are pleased to introduce our community and share our experience. \nDate: 30th Sept 2021 (8:45 P.M. Onwards) \nLast Date of Registration: 29th Sept 2021 \nPlatform: Google Meet",
         link: "https://docs.google.com/forms/d/17vSaUkvuQFQup2xcgy6XUZOAExCLIH7QQXv-oNHYyko/viewform?",
       },
        
@@ -39,11 +36,7 @@ export const event = [
       {
         title: "1 Year Anniversary Celebration (5th June 2021)",
         image: process.env.PUBLIC_URL + "/Assets/Event/past/5.jpg",
-        content: "hello everyone, greetings from abhiyanta community !! we’re happy to announce our 1st year anniversary and invite you to celebrate this auspicious occasion with us. we have great seminars and events lined-up. we’re doing our best to create a unique platform and to provide a practical based learning ecosystem, and the results speak for themselves. \
-        \nwe’re happy to announce our 1st year anniversary and invite you to celebrate this auspicious occasion with us. we have great seminars and events lined-up. we’re doing our best to create a unique platform and to provide a practical based learning ecosystem, and \
-        \nDay 2: “GD on our Education System” \
-        \nDate: 6th June, 2021 (10:00 A.M. to 12:00 P.M.) \
-        \nThe objective behind organizing this thorough discussion on “Improvement in our Education System” is targeting several issues which are required to be taken care of in our education system."
+        content: "hello everyone, greetings from abhiyanta community !! we’re happy to announce our 1st year anniversary and invite you to celebrate this auspicious occasion with us. we have great seminars and events lined-up. we’re doing our best to create a unique platform and to provide a practical based learning ecosystem, and the results speak for themselves. \nwe’re happy to announce our 1st year anniversary and invite you to celebrate this auspicious occasion with us. we have great seminars and events lined-up. we’re doing our best to create a unique platform and to provide a practical based learning ecosystem, and \nDay 2: “GD on our Education System” \nDate: 6th June, 2021 (10:00 A.M. to 12:00 P.M.) \nThe objective behind organizing this thorough discussion on “Improvement in our Education System” is targeting several issues which are required to be taken care of in our education system."
       },
 
       {
@@ -52,13 +45,7 @@ export const event = [
         image: process.env.PUBLIC_URL + "/Assets/Event/past/4.jpg",
 
         content:
-          "Hello Folks, Too bored on vacation !!?  Don’t worry, Abhiyanta Community-Community of innovators are back again with exciting video editing and designing events- An Art of Videography.\
-        \nThis event consist of two exciting rounds,\
-        \nRound I: Video creation and designing challenge in a creative and attractive manner !!\
-        \nRound II: Top 5 candidates will be selected to the next surprising event with deserving rewards!!\
-        \nParticipate now and win exciting prizes with deserving certificates. Don’t miss this thriller opportunity, limited seats are available.\
-        \nDate : 22nd May,  2021- 23rd May, 2021 (48 hrs) \
-        \nLast date of registration: 21st May,2021",
+          "Hello Folks, Too bored on vacation !!?  Don’t worry, Abhiyanta Community-Community of innovators are back again with exciting video editing and designing events- An Art of Videography. \nThis event consist of two exciting rounds, \nRound I: Video creation and designing challenge in a creative and attractive manner !! \nRound II: Top 5 candidates will be selected to the next surprising event with deserving rewards!! \nParticipate now and win exciting prizes with deserving certificates. Don’t miss this thriller opportunity, limited seats are available. \nDate : 22nd May,  2021- 23rd May, 2021 (48 hrs)  \nLast date of registration: 21st May,2021",
       },
       {
         title: "Basic Electronics Battle ",
@@ -66,28 +53,21 @@ export const event = [
         image: process.env.PUBLIC_URL + "/Assets/Event/past/3.jpg",
 
         content:
-          "Too bored of sessionals and submissions! Don’t worry Abhiyanta Community has come up with an awesome electronics based open book challenge. Yes you heard it right, open book challenge. A competition in which you can test your electronic skills and showcase your talent.\
-        \nConducted by - Prit Varmora, Minu Chaudhary, Ritvik Tiwari, Soham Vekariya.\
-        \nPlatform: Google Meet",
+          "Too bored of sessionals and submissions! Don’t worry Abhiyanta Community has come up with an awesome electronics based open book challenge. Yes you heard it right, open book challenge. A competition in which you can test your electronic skills and showcase your talent. \nConducted by - Prit Varmora, Minu Chaudhary, Ritvik Tiwari, Soham Vekariya. \nPlatform: Google Meet",
       },
       {
         title: "e-Yantra awareness program ",
         title2: "(14th August 2020)",
         image: process.env.PUBLIC_URL + "/Assets/Event/past/1.jpg",
         content:
-          "Online webinar to spread awareness regarding various competitions conducted by e-yantra. E-yantra conducts many exciting competitions and hackathons such as eYRC and eYIC. The speakers are 2nd place holder of the eYRC robotics competition in the theme of Biped Patrol.\
-        \nConducted by - Prit Varmora, Ritvik Tiwari \
-        \nPlatform: Google Meet",
+          "Online webinar to spread awareness regarding various competitions conducted by e-yantra. E-yantra conducts many exciting competitions and hackathons such as eYRC and eYIC. The speakers are 2nd place holder of the eYRC robotics competition in the theme of Biped Patrol. \nConducted by - Prit Varmora, Ritvik Tiwari  \nPlatform: Google Meet",
       },
       {
         title: "Convince Investors’ ",
         title2: "(06th December 2020)",
         image: process.env.PUBLIC_URL + "/Assets/Event/past/2.jpg",
         content:
-          "Online competition where teams present their project to potential investors. Teams which were working since past 2 months on industry-level projects get a chance to market their project in front of experienced jury members from the industry.\
-        \nConducted by - Prit Varmora, Minu Chaudhary, Ritvik Tiwari, Soham Vekariya.\
-        \nJury Members - Raj Shingala (Cadence Design System Pratik Gupta (Cadence Design Systems)\
-        \nPlatform: Google Meet",
+          "Online competition where teams present their project to potential investors. Teams which were working since past 2 months on industry-level projects get a chance to market their project in front of experienced jury members from the industry. \nConducted by - Prit Varmora, Minu Chaudhary, Ritvik Tiwari, Soham Vekariya. \nJury Members - Raj Shingala (Cadence Design System Pratik Gupta (Cadence Design Systems) \nPlatform: Google Meet",
       },
     ],
   },
